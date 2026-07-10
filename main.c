8548c85ad680c3dc1a74176fa43e29ddf4899c94
falep9 <felipemotta1515@gmail.com>
2026-07-10 00:58:44 -0300

    Corrige erros de sintaxe e typos

--- main.c
+++ main.c
@@ -12,31 +12,29 @@ int main(){
   while (rodando == 1){
     printf("nshell> ");
 
-
     if (fgets(linha, TAM_MAX, stdin) == NULL){
-      break; //Erro de leitura ou Ctrl + D --> Encerra o shell 
+      break; //Erro de leitura ou Ctrl + D --> Encerra o shell
     }
 
-    // fgets guarda o 'n' no final, então removemos ele
+    // fgets guarda o '\n' no final, então removemos ele
     linha[strcspn(linha, "\n")] = '\0';
 
     // se o user apertou enter, ignora e mostra o prompt de novo
     if (strlen(linha) == 0){
       continue;
     }
-    
-    // quebra a linha digitada em palavras separadas (args[])
-    int argc = parse_linha(linha, args); 
 
+    // quebra a linha digitada em palavras separadas (args[])
+    int argc = parse_linha(linha, args);
     if (argc == 0){
-      continue; 
+      continue;
     }
-    
+
     // tenta executar como o comando interno do shell
     // se nao for builtin, vamos chamar ele futuramente (ls, pwd)
-    
-    int era_builtin = executar_builtin(args, &rodando){
-     if (seila_builtin);
+    int era_builtin = executar_builtin(args, &rodando);
+
+    if (!era_builtin) {
         printf("Comando nao encontrado: %s\n", args[0]);
     }
   }
--- src/builtin.c
+++ src/builtin.c
@@ -7,17 +7,17 @@ void mostrar_ajuda() {
   printf("Comandos disponíves: ajuda, sair\n");
 }
 
-int executar_builtin(chat *args[], int *rodando){
+int executar_builtin(char *args[], int *rodando){
 
   if (strcmp(args[0], "ajuda") == 0){
-    mostrar_ajuda;
+    mostrar_ajuda();
     return 1;
   }
 
   if (strcmp(args[0], "sair") == 0){
-    prinf("Saindo do nshell\n");
-    *rodando 0
-    return 1
+    printf("Saindo do nshell\n");
+    *rodando = 0;
+    return 1;
   }
   
   return 0;
